f8b3ab478f7d3e4969710660ad051de0b24b751c
unknown <mjawwadiqbal22@gmail.com>
2019-02-03 09:50:54 -0800

    --Commit:deleting item

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ class App extends Component {
       
     ]
   }
+  deletTodo=(id)=>{
+    const todos=this.state.todos.filter(todo=>{
+      return todo.id!==id
+    });
+    this.setState({
+      todos:todos
+    })
+  }
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
        <h1 className="center blue-text">Todos's</h1>
-<Todos todos={this.state.todos}/>
+<Todos deleteTodo={this.deletTodo} todos={this.state.todos}/>
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-const Todos=({todos}) =>{
-    const todoList=todos.length ? (
-        todos.map(todo=>{
+const Todos = ({ todos ,deleteTodo }) => {
+    const todoList = todos.length ? (
+        todos.map(todo => {
             return (
-            <div className="collection-item" key={todo.id}>
-                <span>{todo.content}</span>
-            </div>
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={()=>{deleteTodo(todo.id)}}>{todo.content}</span>
+               
+                </div>
+            )
+        })
+    ) : (
+            <p className="center">You have no todos left</p>
         )
-    })
-    ):(
-        <p className="center">You have no todos left</p>
-    )
-    return(
+    return (
         <div className="todos collection">
-{todoList}
+            {todoList}
         </div>
     )
 }
